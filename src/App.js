749079385a4c362268c0d5be7f9b754648adbbba
mateusz <mateusz.basiak@gmail.com>
2019-04-03 01:10:41 +0200

    add search component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
+import Search from './components/search'
 
 import './App.css';
 
 class App extends Component {
+
+
+getKeywords= e=>{
+  let key = e.target.value;
+  console.log(key)
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          naglowek aplikacji
-        
-        </header>
+      
+      <Search keywords={this.getKeywords}/>
+      <h2>booklist</h2>
+      <h2>chossenbook</h2>
       </div>
     );
   }
 }
 
-export default App;
+
+
+export default connect(null,null)(App);
