be9d2e426fa7ca8fc61bbe2a995b5de32759cf40
mateusz <mateusz.basiak@gmail.com>
2019-04-05 23:43:14 +0200

    search debounce added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import { connect } from "react-redux";
 import Search from "./components/search";
 import { bindActionCreators } from "redux";
 import { booksList } from "./actions";
-import { debounce } from "lodash.debounce";
 
 import "./App.css";
 
 class App extends Component {
   printProps = () => {
     let items = this.props.books.items;
-    console.log(items[0].volumeInfo.title)
-    console.log(items[0].volumeInfo.authors)
-    console.log(items[0].volumeInfo.description)
-    console.log(items[0].volumeInfo.imageLinks.thumbnail)
+    console.log(items[0].volumeInfo.title);
+    console.log(items[0].volumeInfo.authors);
+    console.log(items[0].volumeInfo.description);
+    console.log(items[0].volumeInfo.imageLinks.thumbnail);
   };
   getKeywords = e => {
     let key = e.target.value;
@@ -32,7 +31,6 @@ class App extends Component {
   }
 }
 const mapStateToProps = state => {
-  console.log("app", state);
   return {
     books: state.booksList.books
   };
--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,15 @@
 import React from "react";
+import {DebounceInput} from 'react-debounce-input'
+
 
 const Search = props => {
   return (
     <div className="search_container">
       <h2> Podaj tytuł Ksiązki</h2>
-      <input type="text" onChange={event => props.keywords(event)} />
+      {/* <input type="text" onChange={event => props.keywords(event.target.value)} /> */}
+      <DebounceInput minLength={2}
+      debounceTimeout={700}
+        onChange={event => props.keywords(event)} />
     </div>
   );
 };
