af0ea2fd093cbc54704eec36643a791b30ba3ab9
mateusz <mateusz.basiak@gmail.com>
2019-04-07 01:25:32 +0200

    Updates

---
+++ precache-manifest.79e1eefa229e8c9476b70ad74bfeaa17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "56dfa014692b9a785187",
+    "url": "/BookReader/static/css/main.484729a5.chunk.css"
+  },
+  {
+    "revision": "56dfa014692b9a785187",
+    "url": "/BookReader/static/js/main.97aa1332.chunk.js"
+  },
+  {
+    "revision": "d9440b4062a76301573c",
+    "url": "/BookReader/static/js/runtime~main.f3737d11.js"
+  },
+  {
+    "revision": "de8b3fb35fd631543500",
+    "url": "/BookReader/static/js/2.4e512a31.chunk.js"
+  },
+  {
+    "revision": "57da1720c8ec3c5b7c19d6c99a227605",
+    "url": "/BookReader/static/media/no-image.57da1720.jpg"
+  },
+  {
+    "revision": "c22373928eb96f2e5f30063f2714c898",
+    "url": "/BookReader/static/media/background.c2237392.jpg"
+  },
+  {
+    "revision": "fd7829379d760f6488d2a6afd845c298",
+    "url": "/BookReader/index.html"
+  }
+];
